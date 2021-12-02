1d77629173a4c89015dc4f43ea2f96f1401d6973
bosekarthikstarloopstudios <bose.karthik@starloopstudios.com>
2021-12-02 16:44:24 +0530

    Init

---
+++ Source/ApolloTest/ApolloTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ApolloTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APOLLOTEST_API AApolloTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
